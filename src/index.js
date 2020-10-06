1bff8617ea1ce019615967bd394aaa9865913801
danieltremura <daniel.tremura@tarainteractive.com>
2020-10-06 12:25:47 +0300

    Add first loaders to handle css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 alert("HELLO WEBPACK");
 import { run } from './app/app';
+import "./main.css";
+
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 
